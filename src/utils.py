fbfb06e70150ad540a80fe09ad0346cc94267508
MarcoStrijker <101836304+MarcoStrijker@users.noreply.github.com>
2023-12-28 16:17:23 +0100

    Create utils.py
    Contains a progress bar

---
+++ src/utils.py
@@ -0,0 +1,52 @@
+def progress(count: int, total: int, suffix: str = None):
+    """
+    Prints a progress bar to the console. Pycharm does not support this out of the box, so
+    set emulate terminal in output console to true in the run configuration.
+
+    Args:
+        count {int} -- The current count, this is the index + 1 of the current iteration.
+        total {int} -- The total number of iterations.
+
+    Keyword Arguments:
+        suffix {str} -- The suffix to be printed after the progress bar. (default: {None})
+
+    Raises:
+        AssertionError: Raised if count is not an integer.
+        AssertionError: Raised if total is not an integer.
+        AssertionError: Raised if count is negative.
+        AssertionError: Raised if total is not positive.
+        AssertionError: Raised if count is larger than total.
+
+    """
+
+    # TODO: until the progress bar is finished silence and collect the stdout.
+
+    # Quickly check if the arguments are valid.
+    assert isinstance(count, int), "Count must be an integer."
+    assert isinstance(total, int), "Total must be an integer."
+    assert count >= 0, "Count must be positive."
+    assert total > 0, "Total must be positive."
+    assert count <= total, "Count must be smaller or equal to total."
+
+    # If no suffix is given, set it to "progressing".
+    if suffix is None:
+        suffix = "progressing"
+
+    bar_len = 70
+
+    # Calculate the percentage of the progress.
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = str(round(100.0 * count / float(total), 1))
+    # Ensure that the percentage is always 4
+    percents = " " * (4 - len(percents)) + percents
+
+    # Create the progress bar.
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    # Print the progress bar
+    print('[%s] %s%s -- %s%s\r' % (bar, percents, '%', suffix, " " * 24), end='')
+
+    # If the progress is finished, print a newline.
+    if count == total:
+        print()
